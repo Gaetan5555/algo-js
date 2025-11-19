1816a5027d83bf045bab59ccb7dd2081935dfda0
Gaetan <gpie.contact@gmail.com>
2025-11-19 17:13:12 +0100

    Ex 1.x
    This JS file contains Exercice 1.0 , 1.1 , 1.2

---
+++ index.js
@@ -0,0 +1,64 @@
+// alert("Salut Java-Script")
+
+// Exercice 1.0 !!!!!!!!!!!!!!!!!!!!!!!!!!
+var hi = "Salut Java-Script"
+console.log(hi);
+
+console.log("Hello World!");
+
+let age = 25;
+console.log(age);
+age = 26;
+console.log(age);
+
+let phrase = "This is a sentence in a string";
+console.log(phrase);
+
+
+let isittrue = true;
+console.log(isittrue);
+
+let isitfalse = false;
+console.log(isitfalse);
+
+if (isitfalse == 0) {
+    console.log("j'ai réussi");
+} else {
+    console.log("j'ai échoué");
+}
+
+// Exercice 1.1  !!!!!!!!!!!!!!!!!!!!!!!!!!
+
+let ageP = 23;
+let sisterAge = age + 7;
+console.log("Mon âge est de " + ageP + " ans.");
+
+ageP += 10;
+console.log ("Je serais papa à " + ageP + " ans.");
+
+
+let mercedes = 25
+let bmw = 13
+let tesla = 3
+
+let totalCars = mercedes + bmw + tesla;
+console.log("Le total de voitures est de " + totalCars + " voitures.");
+
+let price1 = 9
+let price2 = 3
+let price3 = 30
+
+console.log(price1*price2)
+console.log(price3/price2)
+console.log(price3/9)
+
+
+// Exercice 1.2 !!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+let name = "Lambert"
+let firstName = "Gerard"
+let city = "Paris"
+
+console.log("Your name is " + firstName + " " + name + "and you live in" + " " + city +".")
+
+
