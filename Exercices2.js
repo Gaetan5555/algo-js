7dad8d3893873c5dcad974a68ed90a9ba4e687a8
Gaetan <gpie.contact@gmail.com>
2025-11-20 11:54:18 +0100

    Add files via upload

---
+++ Exercices2.js
@@ -0,0 +1,110 @@
+// Théorie:  How to use :while
+
+// let i = 1;
+
+// while (i <= 100) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// This code displays all numbers from 1 to 100
+
+
+// Operateur d'affectations:  x= += 2; (ajoute 2 à la valeur de x)  // x -= 3; (diminue de 3 la valeur de x)
+// Turoriel  min 49' : https://www.youtube.com/watch?v=Ew7KG2j8eII&t=2931s
+
+// let s = 20;
+// while (s >= 12) {
+//     console.log(s)
+//     s -= 2;
+// }
+
+// Excercices 2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// Exercice 2.1
+
+// let age = 16
+// if (age>=18) {
+// console.log("Your are an adult");
+// }
+// else {
+//     console.log("You are not yet an adult")
+// }
+
+
+
+// Exercice 2.2
+
+// let min = 10
+// let max = 55
+// let current = 72
+
+// if ((current>=min) && (current<=max)) {
+//     console.log(current)
+// }
+// else if (min>max) {
+//     console.log("You made a mistake in the initial input. Please review your notes")
+// }
+// else if ((current<min) || (current>max)) {
+// console.log("You are not in the required age range")
+// }
+
+
+
+// Exercice 2.3.............................
+
+// let evenNumbers = 0
+
+// while (evenNumbers<=100) {
+//     console.log(evenNumbers)
+//     evenNumbers++ 
+// }
+    //NOTE:  Attention if you writte evenNumbers ++ (space between evenNumbers and ++)it will loop for ever !
+    // An alternative to evenNumbers++ is evenNumbers +=1
+
+// for (let numb=0; numb<=100; numb +=2) {
+//     console.log(numb)
+// }
+
+// Exercice 2.4..............................
+
+let count = 1
+
+while (count<100) {
+    count +=1
+    if(count % 2 === 0) {
+        console.log(count/2)
+    }
+    else if (count % 2 !== 0){
+        console.log(count/3)
+    }   
+}
+
+// Exercice 2.5.to Exponential..............
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
